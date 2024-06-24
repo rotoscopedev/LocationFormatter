a3ff1501fd56455d00d34a0c2b04f59c7e6f6d20
Jeff Johnston <hishma@users.noreply.github.com>
2024-06-24 07:17:01 -0700

    Enable strict concurrency checking
    * Major refactoring in support of concurrency checking - included backward breaking changes.
    * Adopt swift testing.

--- Package.swift
+++ Package.swift
@@ -1,10 +1,10 @@
-// swift-tools-version: 5.5
+// swift-tools-version: 5.9
 
 import PackageDescription
 
 let package = Package(
     name: "LocationFormatter",
-    platforms: [.iOS(.v12), .macOS(.v10_13)],
+    platforms: [.iOS(.v12), .macOS(.v10_15)],
     products: [
         .library(
             name: "LocationFormatter",
@@ -12,14 +12,23 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/wtw-software/UTMConversion", from: "1.4.0"),
+        .package(url: "https://github.com/apple/swift-numerics", from: "1.0.0"),
     ],
     targets: [
         .target(
             name: "LocationFormatter",
-            dependencies: [.product(name: "UTMConversion", package: "UTMConversion")]),
+            dependencies: [.product(name: "UTMConversion", package: "UTMConversion")],
+            swiftSettings: [
+                .enableExperimentalFeature("StrictConcurrency")
+            ]
+        ),
         .testTarget(
             name: "LocationFormatterTests",
-            dependencies: ["LocationFormatter"]),
+            dependencies: [
+                "LocationFormatter",
+                .product(name: "Numerics", package: "swift-numerics"),
+            ]
+        ),
     ]
 )
 
